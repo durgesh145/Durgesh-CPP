c1990ec6d4dc1b1e5f335d67a9b3f89cd88f8d9c
Durgesh Kumar <durgesh.kumar@krishnacollege.ac.in>
2021-03-13 15:45:22 +0530

    durgesh

---
+++ even.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int remainder,number ;
+    cout <<"enter a integer  number:";
+    cin >> number;
+    
+    if (number%2=0)
+    {
+        cout<<number << "is a even number";
+        }
+        else {
+            cout<< number << "is a odd number";
+        }
+    return 0;
+}
